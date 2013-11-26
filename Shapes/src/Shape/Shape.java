b2011410bbef26f68aca9b977c50bf2e03d30be7
chenna-krishna <csagiili@asu.edu>
2013-11-25 21:21:34 -0700

    version1

---
+++ Shapes/src/Shape/Shape.java
@@ -0,0 +1,23 @@
+package Shape;
+
+
+public class Shape
+{
+  protected double area;
+  protected double perimeter;
+
+ public Shape()
+ {
+   area=perimeter=0;
+ }
+  public String toString()
+  	 {
+
+  	     return "area = " + area +
+                  " \t perimeter = " + perimeter;
+
+  	 }
+
+
+}
+
